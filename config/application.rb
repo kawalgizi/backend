f6b8c836731b633cd7247eea596ff16ef0184c24
Kris Mulyandani Putra <kris@hijup.com>
2016-12-17 21:56:13 +0700

    Skip assets, test and helpers when using scaffold

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module Backend
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.orm :active_record
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework nil
+    end
   end
 end
