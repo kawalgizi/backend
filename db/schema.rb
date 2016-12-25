7cbe907ebb8ad75e0b6884a9d222bb4c754524b6
detikpw <the2ndflamingheart@gmail.com>
2016-12-25 23:52:33 +0700

    Add some columns to `products` `serving_size` and `serving_per_container`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161225164056) do
+ActiveRecord::Schema.define(version: 20161225165144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,11 @@ ActiveRecord::Schema.define(version: 20161225164056) do
   create_table "products", force: :cascade do |t|
     t.string   "bpom_id"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.integer  "brand_id"
+    t.decimal  "serving_size"
+    t.integer  "serving_per_container"
     t.index ["brand_id"], name: "index_products_on_brand_id", using: :btree
   end
 
