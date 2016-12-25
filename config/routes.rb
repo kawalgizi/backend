022fee9fbf8e427ef7345a935b586137203b74e2
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2016-12-25 16:50:59 +0700

    Add CRUD user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   devise_for :users
-  resources :tweets
-  root to: 'tweets#index'
+  resources :tweets, path: 'admin/tweets'
+  resources :users, path: 'admin/users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
