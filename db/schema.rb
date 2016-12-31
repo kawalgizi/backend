4764e6dcca80f8fcd3211ebfbcfbaf7cfa30cb66
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2016-12-31 17:10:48 +0700

    Remove Tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226020058) do
+ActiveRecord::Schema.define(version: 20161231100854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "tweets", force: :cascade do |t|
-    t.string   "username"
-    t.text     "tweet"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
