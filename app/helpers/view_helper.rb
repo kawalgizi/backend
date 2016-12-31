8e4d6aad524f9f455276fcb5f6f9525a09d09696
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2016-12-31 16:28:42 +0700

    Change order params in sidebar_link helper

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,5 +1,5 @@
 module ViewHelper
-  def sidebar_link(icon, text, target_path)
-    link_to raw("<i class='fa #{icon}'></i> <span> #{text} </span>"), target_path
+  def sidebar_link(text, path, icon)
+    link_to raw("<i class='fa #{icon}'></i> <span> #{text} </span>"), path
   end
 end
