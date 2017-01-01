f10d07a424dae491196527dcf479513008afb541
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2017-01-01 16:27:51 +0700

    Applied admin lte style in sign in and sign up

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Admin::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
-layout 'auth'
+layout 'auth', except: [:edit]
 
   # GET /resource/sign_up
   # def new
